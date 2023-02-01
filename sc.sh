deb21f273747c71a7bfd67288429fa9ad54e3eff
fhwhite <47919090+fhwhite@users.noreply.github.com>
2023-02-01 12:02:43 +0800

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 screen_name="test_screen" 
 screen -dmS $screen_name
-screen -x -S $screen_name -p 0 -X stuff "bash /root/mem.sh"
+screen -x -S $screen_name -p 0 -X stuff "bash /root/mem.sh
+"
