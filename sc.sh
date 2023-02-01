251e09654ed31d2963b554d395cb33802f3776ef
fhwhite <47919090+fhwhite@users.noreply.github.com>
2023-02-01 12:09:30 +0800

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-rm /root/mem.sh
-rm /root/sc.sh
-rm /root/speedtest.py
+cd /root
+rm mem.sh
+rm sc.sh
+rm speedtest.py
 wget https://raw.githubusercontent.com/fhwhite/oramem/main/mem.sh
 wget https://raw.githubusercontent.com/fhwhite/oramem/main/speedtest.py
 screen_name="test_screen" 
